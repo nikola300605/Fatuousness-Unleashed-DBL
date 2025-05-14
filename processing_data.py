6bb92a36fbf63e224796f9153c629eb41d5aa581
Göktürk Kalender <goktrk04@gmail.com>
2025-05-14 19:26:45 +0200

    Creating airline ids for future use, and creating mining function that stores the entire tweet object by their id.

--- processing_data.py
+++ processing_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 from bson import json_util, ObjectId
 import json
 from pandas import json_normalize
+from pymongo_interface import get_mongo_collection
+from dateutil import parser
 
 def process_batch(batch):
     sanitised = json.loads(json_util.dumps(batch))
@@ -10,3 +12,23 @@ def process_batch(batch):
     df = pd.DataFrame(normalised)
 
     return df
+
+#Defining Airline IDs for checking the match of the Tweet
+
+AIRLINE_IDS = {
+    56377143, 106062176, 18332190, 22536055, 124476322, 26223583,
+    2182373406, 38676903, 1542862735, 253340062, 218730857, 45621423,
+    20626359
+}
+
+def mine_conversations():
+    collection = get_mongo_collection()
+    tweets = list(collection.find({}))
+    tweet_by_id = {}
+
+    for tweet in tweets:
+        if "id" in tweet:
+            tweet_by_id[tweet["id"]] = tweet
+
+    conversations = []
+    skipped = 0
