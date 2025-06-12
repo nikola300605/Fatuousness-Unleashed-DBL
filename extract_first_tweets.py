67951d16750713e7e494c976f0bc3d884bcbdab9
Vedat Yalçin <v20yalcin@gmail.com>
2025-06-12 16:07:52 +0200

    complete extract first tweet pi

--- extract_first_tweets.py
+++ extract_first_tweets.py
@@ -13,5 +13,16 @@ for convo in collection.find({}):
     all_texts = [msg["text"] for msg in thread]
 
     output.append({
-        
+        "converstion_id": str(convo["_id"]),
+        "airline": convo.get("airline", None),
+        "first_tweet": first_tweet,
+        "full_convo": all_texts,
+        "participants": convo.get("participants",[]),
+        "length": convo.get("length", len(all_texts)),
+        "start_sent": convo.get("computed_metrics", {}).get("start_sent",None),
+        "end_sent": convo.get("computed_metrics", {}).get("end_sent",None),
+        "delta_sent": convo.get("computed_metrics", {}).get("delta_sent",None)
     })
+
+with open("conversation_data_cleaned.json","w") as f:
+    json.dump(output, f, indent=2)
