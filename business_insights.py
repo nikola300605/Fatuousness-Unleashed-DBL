9b6e6822b111621e5c7c43b26effbd210468d0f3
Vedat Yalçin <v20yalcin@gmail.com>
2025-06-18 15:46:23 +0200

    businees insight topic classification

--- business_insights.py
+++ business_insights.py
@@ -9,4 +9,17 @@ df = pd.DataFrame(data)
 
 def classify_topic(text):
     text = text.lower()
+    if re.search(r"delay|late|cancel|reschedule",text):
+        return "delay"
+    elif re.search(r"luggage|bag|lost|baggage",text):
+        return "luggage"
+    elif re.search(r"booking|ticket|seat|check[- ]in",text):
+        return "booking"
+    elif re.search(r"rude|staff|customer service|support|agent",text):
+        return "customer_service"
+    else:
+        return "other"
     
+df["topic"] = df["first_tweet"].apply(classify_topic)
+
+df["resolved"] = df["delta_sent"] > 0
